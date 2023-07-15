25619c431e253ca5906fbc9b7b46da11b0009a83
VPRAVEEN <VPRAVEEN@INLEN8520042297.corp.capgemini.com>
2023-07-15 17:27:27 +0530

    First Commit

--- src/test/java/com/cg/hms/HmsApplicationTests.java
+++ src/test/java/com/cg/hms/HmsApplicationTests.java
@@ -1,13 +1,41 @@
-package com.cg.hms;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class HmsApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.cg.hms;
+//import org.junit.jupiter.api.Test;
+//import static org.junit.jupiter.api.Assertions.assertThrows;
+//import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import com.cg.hms.entities.Nurse;
+//import com.cg.hms.exceptions.DuplicateEntryException;
+//import com.cg.hms.exceptions.NoRecordsException;
+//import com.cg.hms.services.NurseServiceImpl;
+//
+//@SpringBootTest
+//class HmsApplicationTests {
+//private NurseServiceImpl service;
+//@Autowired
+//public void setService(NurseServiceImpl service) {
+//	this.service = service;
+//}
+//	@Test
+//	void contextLoads() {
+//		
+//	}
+//	@Test
+//	void getAllNurses() {
+//	assertDoesNotThrow(()->service.getallNurses());
+//	}
+//	@Test
+//	void saveNurse() {
+//		Nurse nurse = new Nurse(670,"sunaina","HeadNurse",true,120);
+//		assertDoesNotThrow(()-> service.saveNurse(nurse));
+//		
+//	}
+//	@Test
+//	void nurseSaveException() {
+//		Nurse nurse = new Nurse(670,"sunaina","HeadNurse",true,120);
+//		assertThrows(DuplicateEntryException.class,()->{service.saveNurse(nurse);
+//	});
+//
+//}
+//}
